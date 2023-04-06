00216aa6391809bd3e38c7b6416fa197689f04b9
NicMilli <nicbass2@gmail.com>
2023-04-05 20:10:37 -0700

    Importing all .csv files into tables

--- server/db.js
+++ server/db.js
@@ -11,36 +11,48 @@ const pool = new Client({
 const connect = async () => {
   await pool.connect();
 
-  await pool.query("DROP TABLE product");
+  await pool.query('DROP TABLE IF EXISTS product');
+  await pool.query('DROP TABLE IF EXISTS styles');
+  await pool.query('DROP TABLE IF EXISTS skus');
+  await pool.query('DROP TABLE IF EXISTS photos');
 
+  // Create product table
   await pool.query('CREATE TABLE IF NOT EXISTS product (product_id INTEGER NULL DEFAULT NULL, name VARCHAR(50) NOT NULL, slogan VARCHAR(200) NULL DEFAULT NULL, description VARCHAR(500) DEFAULT NULL, category VARCHAR(25) DEFAULT NULL, default_price DECIMAL(10, 2) DEFAULT NULL, PRIMARY KEY (product_id))');
 
-  await pool.query("COPY product FROM '/Library/product.csv' DELIMITER ',' CSV");
-
+  // product_features table
   await pool.query('CREATE TABLE IF NOT EXISTS product_features (product_id INTEGER NULL DEFAULT NULL, feature INTEGER NULL DEFAULT NULL, PRIMARY KEY (product_id))');
 
+  // features table
   await pool.query('CREATE TABLE IF NOT EXISTS features (feature_id INTEGER NULL DEFAULT NULL, feature VARCHAR(20) NULL DEFAULT NULL, value VARCHAR(20) NULL DEFAULT NULL, PRIMARY KEY (feature_id))');
 
-  await pool.query('CREATE TABLE IF NOT EXISTS features (product_id INTEGER NULL AUTO_INCREMENT DEFAULT NULL, results INTEGER NULL DEFAULT NULL, PRIMARY KEY (product_id), KEY (product_id))');
+  // styles table
+  await pool.query('CREATE TABLE IF NOT EXISTS styles (style_id INTEGER NULL DEFAULT NULL, product_id INTEGER NULL DEFAULT NULL, name VARCHAR(20) NULL DEFAULT NULL, sale_price DECIMAL((10, 2)) NULL DEFAULT NULL, default? BIT NULL DEFAULT NULL, PRIMARY KEY (style_id))');
 
-  await pool.query('CREATE TABLE IF NOT EXISTS results (`style_id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL, name VARCHAR(20) NULL DEFAULT NULL, original_price DECIMAL((10, 2)) NULL DEFAULT NULL, sale_price DECIMAL((10, 2)) NULL DEFAULT NULL, photos SET NULL DEFAULT NULL, skus INTEGER NULL DEFAULT NULL, default? BIT NULL DEFAULT NULL, PRIMARY KEY (style_id))');
+  await pool.query('CREATE TABLE IF NOT EXISTS skus (sku_id INTEGER NULL DEFAULT NULL, style_id INTEGER NULL DEFAULT NULL, size VARCHAR(5) NULL DEFAULT NULL, quantity INTEGER NULL DEFAULT NULL)');
+
+  await pool.query('CREATE TABLE IF NOT EXISTS photos (photo_id INTEGER NULL DEFAULT NULL, style_id INTEGER NULL DEFAULT NULL, url VARCHAR(110) NULL DEFAULT NULL, thumbnail_url VARCHAR(110) NULL DEFAULT NULL)');
+
+  await pool.query('CREATE TABLE IF NOT EXISTS related (related_id INTEGER NULL DEFAULT NULL, current_product_id INTEGER NULL DEFAULT NULL, related_product_id INTEGER NULL DEFAULT NULL)');
+
+  // COPY existing data into table
+  await pool.query("COPY product FROM '/Library/product.csv' DELIMITER ',' CSV");
+
+  // COPY existing data into table
+  await pool.query("COPY styles FROM '/Library/styles.csv' DELIMITER ',' CSV");
+
+  // COPY existing data into table
+  await pool.query("COPY skus FROM '/Library/skus.csv' DELIMITER ',' CSV");
+
+  // COPY existing data into table
+  await pool.query("COPY photos FROM '/Library/photos.csv' DELIMITER ',' CSV");
+
+  // COPY existing data into table
+  await pool.query("COPY related FROM '/Library/related.csv' DELIMITER ',' CSV");
 
   const rows = await pool.query('SELECT * FROM product');
   console.log(rows.rows);
 };
 
 connect();
-
-// db.queryAsync('CREATE TABLE IF NOT EXISTS users (id INT NOT NULL PRIMARY KEY, sessionId VARCHAR(50) UNIQUE)')
-//   .then((result) => {
-//     console.log(result);
-//   })
-//   .then(() => {
-//     // COPY products FROM '/path/to/csv/products' DELIMITER ',' CSV;
-//     return db.queryAsync('SELECT * FROM users');
-//   })
-//   .then((ret) => {
-//     console.log(ret.rows)
-//   });
 
 module.exports = pool;
