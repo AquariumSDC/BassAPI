a8ae1cea920b0c4d3dc3226f3a15137f0cdfda7f
NicMilli <nicbass2@gmail.com>
2023-04-06 22:53:26 -0700

    getOne query working, get styles not operational

--- server/controller.js
+++ server/controller.js
@@ -2,21 +2,39 @@ const model = require('./model');
 
 module.exports = {
   getStyles: async (req, res) => {
+    // try {
+    //   const styles = await model.getStyles(req.params.product_id);
+    //   res.status(200).send(styles);
+    // } catch (error) {
+    //   res.status(404).send(error);
+    // }
     const styles = await model.getStyles(req.params.product_id);
     res.status(200).send(styles);
   },
   getRelated: async (req, res) => {
-    const related = await model.getRelated(req.params.product_id);
-    res.status(200).send(related);
+    try {
+      const related = await model.getRelated(req.params.product_id);
+      res.status(200).send(related);
+    } catch (error) {
+      res.status(404).send(error);
+    }
   },
   getOne: async (req, res) => {
-    const product = await model.getOne(req.params.product_id);
-    res.status(200).send(product);
+    try {
+      const product = await model.getOne(req.params.product_id);
+      res.status(200).send(product);
+    } catch (error) {
+      res.status(404).send(error);
+    }
   },
   getAll: async (req, res) => {
     const page = req.query.page || 1;
     const count = req.query.count || 5;
-    const allProducts = await model.getAll(page, count);
-    res.status(200).send(allProducts);
+    try {
+      const allProducts = await model.getAll(page, count);
+      res.status(200).send(allProducts);
+    } catch (error) {
+      res.status(404).send(error);
+    }
   },
 };
--- server/model.js
+++ server/model.js
@@ -2,9 +2,10 @@ const db = require('./db');
 
 module.exports = {
   getStyles: async (productId) => {
-    const queryString = `SELECT * FROM styles WHERE product_id=${productId}`;
+    // const queryString = `SELECT * FROM styles WHERE product_id=${productId}`;
+    const queryString = `SELECT jsonb_build_object('style_id', style_id, 'name', name, 'original_price', original_price, 'sale_price', sale_price, 'default?', default?, 'photos', (SELECT jsonb_agg(jsonb_build_object('thumbnail_url', photos.thumbnail_url, 'url', photos.url)) FROM photos WHERE style_id=style.style_id) AS style FROM styles WHERE product_id=${productId}`;
     const styles = await db.pool.query(queryString);
-    return styles.rows;
+    return { product_id: productId, results: styles.rows };
   },
   getRelated: async (productId) => {
     const queryString = `SELECT * FROM related WHERE product_id=${productId}`;
@@ -12,13 +13,10 @@ module.exports = {
     return related.rows;
   },
   getOne: async (productId) => {
-    const productQueryString = `SELECT * FROM product WHERE id=${productId}`;
-    const featuresQueryString = `SELECT (feature, value) FROM features WHERE product_id=${productId}`;
+    const queryString = `SELECT jsonb_build_object('id', id, 'name', name, 'slogan', slogan, 'description', description, 'category', category, 'default_price', default_price, 'features', (SELECT jsonb_agg(jsonb_build_object('feature', feature, 'value', value)) FROM features WHERE product_id=${productId})) FROM product WHERE id=${productId}`;
+    const product = await db.pool.query(queryString);
 
-    const product = await db.pool.query(productQueryString);
-    const features = await db.pool.query(featuresQueryString);
-    console.log(features.rows[0].row)
-    return product.rows;
+    return product.rows[0].jsonb_build_object;
   },
   getAll: async (page, count) => {
     const queryString = `SELECT * FROM product LIMIT ${count} OFFSET ${(page - 1) * count}`;
