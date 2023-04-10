5f18ba0eb794def6c597f32553b4505a9e8d37ec
NicMilli <nicbass2@gmail.com>
2023-04-10 09:49:40 -0700

    Local stress tests setup

--- server/tests/loadTest.js
+++ server/tests/loadTest.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 const productIndexes = () => {
-  const range = 1000;
+  const range = 10000;
   const min = 1;
   return Math.round(Math.random() * range + min);
 };
@@ -12,27 +12,22 @@ export const options = {
     my_scenario1: {
       executor: 'constant-arrival-rate',
       duration: '30s', // total duration
-      preAllocatedVUs: 30000, // to allocate runtime resources
+      preAllocatedVUs: 8000, // to allocate runtime resources
 
-      rate: 10, // number of constant iterations given `timeUnit`
+      rate: 5000, // number of constant iterations given `timeUnit`
       timeUnit: '1s',
     },
   },
 };
 
-// export const options = {
-//   vus: 2000,
-//   duration: '30s',
-// };
-
 export default function () {
   const id = productIndexes();
   http.get('http://localhost:8080/api/products');
-  sleep(0.5);
+  sleep(0.1);
   http.get(`http://localhost:8080/api/products/${id}`);
-  sleep(0.5);
+  sleep(0.1);
   http.get(`http://localhost:8080/api/products/${id}/styles`);
-  sleep(0.5);
+  sleep(0.1);
   http.get(`http://localhost:8080/api/products/${id}/related`);
-  sleep(0.5);
+  sleep(0.2);
 }
