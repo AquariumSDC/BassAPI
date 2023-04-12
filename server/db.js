5f08f0ce07a4d5cc123eca25e9bb32b71668e454
NicMilli <nicbass2@gmail.com>
2023-04-12 11:56:00 -0700

    DB connection changed to match env

--- server/db.js
+++ server/db.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.USER,
-  host: process.env.HOST,
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
   database: process.env.DATABASE,
   password: process.env.PASSWORD,
   port: process.env.DBPORT,
