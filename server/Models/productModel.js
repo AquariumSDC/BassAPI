b259d320966875dd0bc1047334e7654858a8b1ad
NicMilli <nicbass2@gmail.com>
2023-09-08 20:22:05 -0700

    stash

--- server/Models/productModel.js
+++ server/Models/productModel.js
@@ -2,8 +2,10 @@ const db = require('../db');
 
 module.exports = {
   getStylesJSON: async (productId, client) => {
+    // q: How do i solve the check ungrouped columns walker error below?
+    // a: https://stackoverflow.com/questions/47263673/ungrouped-columns-error-in-postgresql
     const query = {
-      text: "SELECT jsonb_agg(jsonb_build_object('style_id', style_id, 'name', name, 'original_price', default_price, 'sale_price', sale_price, 'default?', default_style, 'photos', (SELECT jsonb_agg(jsonb_build_object('thumbnail_url', thumbnail_url, 'url', url)) FROM photos WHERE photos.style_id=styles.style_id), 'skus', (SELECT jsonb_object_agg(sku_id, jsonb_build_object('quantity', quantity, 'size', size)) FROM skus WHERE skus.style_id=styles.style_id))) FROM styles WHERE product_id=$1",
+      text: "SELECT jsonb_agg(jsonb_build_object('style_id', style_id, 'name', name, 'original_price', default_price, 'sale_price', sale_price, 'default?', default_style, 'photos', (SELECT jsonb_agg(jsonb_build_object('thumbnail_url', thumbnail_url, 'url', url)) FROM photos WHERE photos.style_id=styles.style_id), 'skus', (SELECT jsonb_object_agg(sku_id, jsonb_build_object('quantity', quantity, 'size', size)) FROM skus WHERE skus.style_id=styles.style_id))) FROM styles WHERE product_id=$1;",
       values: [productId],
     };
     try {
