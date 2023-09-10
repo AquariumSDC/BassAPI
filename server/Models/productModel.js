37c1a0d20e47fa26262c1efbdcb2484ea1f22da8
NicMilli <nicbass2@gmail.com>
2023-09-09 18:38:48 -0700

    Proper formatting

--- server/Models/productModel.js
+++ server/Models/productModel.js
@@ -4,48 +4,49 @@ module.exports = {
   getStylesJSON: async (productId, client) => {
     const query = {
       text: `
-    SELECT
-      s.product_id AS product_id,
-      jsonb_agg(
-        jsonb_build_object(
-          'style_id', s.style_id,
-          'name', s.name,
-          'original_price', s.default_price,
-          'sale_price', s.sale_price,
-          'default?', s.default_style,
-          'photos', (
-            SELECT jsonb_agg(
-              jsonb_build_object(
-                'thumbnail_url', p.thumbnail_url,
-                'url', p.url
+      SELECT jsonb_build_object(
+        'product_id', s.product_id,
+        'results', jsonb_agg(
+          jsonb_build_object(
+            'style_id', s.style_id,
+            'name', s.name,
+            'original_price', s.default_price,
+            'sale_price', s.sale_price,
+            'default?', s.default_style,
+            'photos', (
+              SELECT jsonb_agg(
+                jsonb_build_object(
+                  'thumbnail_url', p.thumbnail_url,
+                  'url', p.url
+                )
               )
-            )
-            FROM photos p
-            WHERE p.style_id = s.style_id
-          ),
-          'skus', (
-            SELECT jsonb_object_agg(
-              sku_id::text,
-              jsonb_build_object(
-                'quantity', sku.quantity,
-                'size', sku.size
+              FROM photos p
+              WHERE p.style_id = s.style_id
+            ),
+            'skus', (
+              SELECT jsonb_object_agg(
+                sku_id,
+                jsonb_build_object(
+                  'quantity', sku.quantity,
+                  'size', sku.size
+                )
               )
+              FROM skus sku
+              WHERE sku.style_id = s.style_id
             )
-            FROM skus sku
-            WHERE sku.style_id = s.style_id
           )
         )
-      ) AS results
-    FROM styles s
-    WHERE s.product_id = $1
-    GROUP BY s.style_id, s.product_id
-    ORDER BY s.style_id ASC;
-  `,
+      ) AS result
+      FROM styles s
+      WHERE s.product_id = $1
+      GROUP BY s.product_id;
+    `,
       values: [productId],
     };
+
     try {
-      const styles = await client.query(query);
-      return styles.rows;
+      const result = await client.query(query);
+      return result.rows[0].result;
     } catch (err) {
       return err;
     }
